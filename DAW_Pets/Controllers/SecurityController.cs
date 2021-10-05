3594e0fd6616dde3371f28bf4f0cfc616e6e05f3
Jorginho <jrg.jmnz.prd@gmail.com>
2021-10-05 15:38:33 -0500

    [COMMIT] SecurityController Recuperado.

---
+++ DAW_Pets/Controllers/SecurityController.cs
@@ -0,0 +1,30 @@
+﻿using DAW_Pets.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAW_Pets.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: SecurityController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string user, string pwd)
+        {
+            var db = new DBESANDWContext();
+            if (db.Usuario.Any(x => x.Login.Equals(user) && x.Password.Equals(pwd))) {
+                return RedirectToAction();
+            }
+            return View("Index");
+        }
+        
+    }
+}
